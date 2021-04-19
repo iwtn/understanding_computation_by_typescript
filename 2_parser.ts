b16a7ec3cfe8db4caeb29af008aff6ea64bac664
iwtn <yasuaki.iwata@livesense.co.jp>
2021-04-19 16:54:29 +0900

    真偽値を追加

--- 2_parser.ts
+++ 2_parser.ts
@@ -112,3 +112,53 @@ class Machine {
 }
 
 (new Machine(new Add(new Multiply(new Nmbr(1), new Nmbr(2)), new Multiply(new Nmbr(3), new Nmbr(4))))).run()
+
+class Bool {
+  constructor(public value: boolean) {
+  }
+
+  inspect(): string {
+    return `<<${this.value}>>`
+  }
+
+  toString(): string {
+    return `${this.value}`
+  }
+
+  isReducible() : boolean {
+    return false
+  }
+
+  reduce(): any {
+  }
+}
+
+class LessThan extends Bool {
+  constructor(private left: Nmbr, private right: Nmbr) {
+    super(left.value < right.value)
+  }
+
+  inspect(): string {
+    return `<<${this.left.toString()} < ${this.right.toString()}>>`
+  }
+
+  toString(): string {
+    return `${this.left} < ${this.right}`
+  }
+
+  isReducible(): boolean {
+    return true
+  }
+
+  reduce(): any {
+    if (this.left.isReducible()) {
+      return new LessThan(this.left.reduce(), this.right)
+    } else if (this.right.isReducible()) {
+      return new LessThan(this.left, this.right.reduce())
+    } else {
+      return new Bool(this.left.value < this.right.value)
+    }
+  }
+}
+
+(new Machine(new LessThan(new Nmbr(5), new Add(new Nmbr(2), new Nmbr(2))))).run()
