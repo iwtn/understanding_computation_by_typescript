6b7a8cd2b0a333e915c3bedc64f7a09929c13474
iwtn <yasuaki.iwata@livesense.co.jp>
2021-07-26 15:48:00 +0900

    NFA実装

--- nfa.ts
+++ nfa.ts
@@ -19,7 +19,7 @@ class NFARulebook {
   constructor(private rules: FARule[]) {
   }
 
-  nextState(states: Set<number>, character: string): Set<number> {
+  nextStates(states: Set<number>, character: string): Set<number> {
     const results:number[][] = []
     for (const state of states) {
       results.push(this.followRulesFor(state, character).flat())
@@ -42,7 +42,6 @@ class NFARulebook {
   }
 }
 
-
 const rulebook: NFARulebook = new NFARulebook([
    new FARule(1, 'a', 1),
    new FARule(1, 'b', 1),
@@ -53,6 +52,68 @@ const rulebook: NFARulebook = new NFARulebook([
    new FARule(3, 'b', 4)
  ])
 console.log(rulebook)
-console.log(rulebook.nextState(new Set([1]), 'b'))
-console.log(rulebook.nextState(new Set([1, 2]), 'a'))
-console.log(rulebook.nextState(new Set([1, 3]), 'b'))
+console.log(rulebook.nextStates(new Set([1]), 'b'))
+console.log(rulebook.nextStates(new Set([1, 2]), 'a'))
+console.log(rulebook.nextStates(new Set([1, 3]), 'b'))
+
+
+class NFA {
+  constructor(private currentStates: Set<number>, private acceptStates: Set<number>, private ruleBook: NFARulebook) {
+  }
+
+  isAccepting(): boolean {
+    for (const currentState of this.currentStates) {
+      for (const acceptState of this.acceptStates) {
+        if (currentState == acceptState) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  readCharacter(character: string): void {
+    this.currentStates = this.ruleBook.nextStates(this.currentStates, character)
+  }
+
+  readString(characters: string): void {
+    for (const char of characters) {
+      this.readCharacter(char)
+    }
+  }
+}
+
+console.log("\n NFA")
+const nfa = new NFA(new Set([1]), new Set([4]), rulebook)
+console.log(nfa.isAccepting())
+nfa.readCharacter('b')
+console.log(nfa.isAccepting())
+nfa.readCharacter('a')
+console.log(nfa.isAccepting())
+nfa.readCharacter('b')
+console.log(nfa.isAccepting())
+
+const nfa2 = new NFA(new Set([1]), new Set([4]), rulebook)
+console.log(nfa2.isAccepting())
+nfa2.readString('bbbbb')
+console.log(nfa2.isAccepting())
+
+class NFADesign {
+  constructor(private startState: number, private acceptStates: Set<number>, private rulebook: NFARulebook) {
+  }
+
+  toNfa(): NFA {
+    return new NFA(new Set([this.startState]), this.acceptStates, this.rulebook)
+  }
+
+  isAccepts(characters: string): boolean {
+    const nfa: NFA = this.toNfa()
+    nfa.readString(characters)
+    return nfa.isAccepting()
+  }
+}
+
+console.log("\n NFADesign")
+const nfaDesign = new NFADesign(1, new Set([4]), rulebook)
+console.log(nfaDesign.isAccepts('bbbbb'))
+console.log(nfaDesign.isAccepts('bbabb'))
