0916fecf9dcc733cf56eb0d51ecaaf29a195856c
iwtn <yasuaki.iwata@livesense.co.jp>
2022-03-15 15:58:10 +0900

    DPDARulebookまで作った

--- pda.test.ts
+++ pda.test.ts
@@ -1,10 +1,31 @@
 import { Stack } from './stack'
-import { PDARule, PDAConfiguration } from './pda'
+import { PDARule, PDAConfiguration, DPDARulebook } from './pda'
 
-test('PDARule', () =>{
-  const rule = new PDARule(1, '(', 2, '$', ['b', '$'])
+test('PDARule', () => {
+  const rule = new PDARule<string>(1, '(', 2, '$', ['b', '$'])
   const configuration = new PDAConfiguration<string>(1, new Stack(['$']))
 
   expect(rule.appliesTo(configuration, '(')).toBe(true)
   expect(rule.appliesTo(configuration, ')')).toBe(false)
+})
+
+
+const rulebook = new DPDARulebook([
+  new PDARule<string>(1, '(', 2, '$', ['b', '$']),
+  new PDARule<string>(2, '(', 2, 'b', ['b', 'b']),
+  new PDARule<string>(2, ')', 2, 'b', []),
+  new PDARule<string>(2, null, 1, '$', ['$']),
+])
+
+test('PDAConfiguration', () => {
+  const conf = new PDAConfiguration<string>(1, new Stack(['$']))
+
+  const conf2 = rulebook.nextConfiguration(conf, '(')
+  expect(conf2.stack.top()).toBe('b')
+
+  const conf3 = rulebook.nextConfiguration(conf2, '(')
+  expect(conf3.stack.top()).toBe('b')
+
+  const conf4 = rulebook.nextConfiguration(conf3, ')')
+  expect(conf4.stack.top()).toBe('b')
 })
--- pda.ts
+++ pda.ts
@@ -5,11 +5,41 @@ export class PDAConfiguration<T> {
   }
 }
 
-export class PDARule {
-  constructor(public state: any, public character: string, public nextState: any, public popCharacter: string, public pushCharacters: string[]) {
+export class PDARule<T> {
+  constructor(public state: any, public character: T, public nextState: any, public popCharacter: T, public pushCharacters: T[]) {
   }
 
-  appliesTo(configuration: PDAConfiguration<string>, character: any): boolean {
+  appliesTo(configuration: PDAConfiguration<T>, character: T): boolean {
     return this.state == configuration.state && this.popCharacter == configuration.stack.top() && this.character == character
+  }
+
+  follow(configuration: PDAConfiguration<T>): PDAConfiguration<T> {
+    return new PDAConfiguration<T>(this.nextState, this.nextStack(configuration))
+  }
+
+  nextStack(configuration: PDAConfiguration<T>): Stack<T> {
+    let popedStack = configuration.stack.pop()
+    const rpc = this.pushCharacters.concat().reverse();
+    for (const c of rpc) {
+      popedStack = popedStack.push(c)
+    }
+    return popedStack
+  }
+}
+
+export class DPDARulebook<T> {
+  constructor(public rules: PDARule<T>[]) {
+  }
+
+  nextConfiguration(configuration: PDAConfiguration<T>, character: T): PDAConfiguration<T> {
+    return this.ruleFor(configuration, character).follow(configuration)
+  }
+
+  ruleFor(configuration: PDAConfiguration<T>, character: T): PDARule<T> {
+    for(const rule of this.rules) {
+      if (rule.appliesTo(configuration, character)) {
+        return rule
+      }
+    }
   }
 }
