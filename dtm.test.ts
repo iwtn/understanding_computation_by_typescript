c1ec2b77e17e88f7ab30d5d535e8904488abd2ef
iwtn <yasuaki.iwata@livesense.co.jp>
2022-09-27 15:16:40 +0900

    テープを左に動かす

--- dtm.test.ts
+++ dtm.test.ts
@@ -1,11 +1,19 @@
 import { Tape } from './dtm';
 
-test('Tape#inspect', () => {
-  const tape = new Tape('101', '1', '')
+test('Tape inspect', () => {
+  const tape = new Tape('101', '1', '', '_')
   expect(tape.inspect()).toBe('101(1)')
 })
 
-test('Tape#write', () => {
-  const tape2 = new Tape('101', '1', '')
+test('Tape write', () => {
+  const tape2 = new Tape('101', '1', '', '_')
   expect(tape2.write('0').inspect()).toBe('101(0)')
+})
+
+test('Tape moveHeadLeft', () => {
+  const tape3 = new Tape('101', '1', '', '_')
+  expect(tape3.moveHeadLeft().inspect()).toBe('10(1)1')
+
+  const tape4 = new Tape('', '1', '011', '_')
+  expect(tape4.moveHeadLeft().inspect()).toBe('(_)1011')
 })
--- dtm.ts
+++ dtm.ts
@@ -1,5 +1,5 @@
 export class Tape {
-  constructor(private left: string, private middle: string, private right: string) {
+  constructor(private left: string, private middle: string, private right: string, private blank: string) {
   }
 
   inspect(): string {
@@ -7,6 +7,11 @@ export class Tape {
   }
 
   write(character: string): Tape {
-    return new Tape(this.left, character, this.right)
+    return new Tape(this.left, character, this.right, this.blank)
+  }
+
+  moveHeadLeft(): Tape {
+    const newMiddle = (this.left.length == 0) ? this.blank : this.left[this.left.length - 1]
+    return new Tape(this.left.substr(0, this.left.length - 1), newMiddle, this.middle + this.right, this.blank)
   }
 }
