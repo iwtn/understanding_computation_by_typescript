af82e87b97c7850dfd9951142fbed5a37ccf4863
iwtn <yasuaki.iwata@livesense.co.jp>
2022-09-27 15:55:58 +0900

    テープヘッドの文字と現在の状態が一致した場合を判定

--- dtm.test.ts
+++ dtm.test.ts
@@ -1,4 +1,4 @@
-import { Tape } from './dtm';
+import { Tape, TMRule, TMConfiguration } from './dtm';
 
 let tape = new Tape('101', '1', '', '_')
 
@@ -21,4 +21,12 @@ test('Tape moveHeadRight', () => {
   let tape3 = tape.moveHeadRight()
   expect(tape3.inspect()).toBe('1011(_)')
   expect(tape3.write('0').inspect()).toBe('1011(0)')
+})
+
+let rule = new TMRule(1, '0', 2, '1', 'right')
+
+test('TMRule', () => {
+  expect(rule.isAppliesTo(new TMConfiguration(1, new Tape('', '0', '', '_')))).toBe(true)
+  expect(rule.isAppliesTo(new TMConfiguration(1, new Tape('', '1', '', '_')))).toBe(false)
+  expect(rule.isAppliesTo(new TMConfiguration(2, new Tape('', '0', '', '_')))).toBe(false)
 })
--- dtm.ts
+++ dtm.ts
@@ -1,5 +1,5 @@
 export class Tape {
-  constructor(private left: string, private middle: string, private right: string, private blank: string) {
+  constructor(private left: string, public middle: string, private right: string, private blank: string) {
   }
 
   inspect(): string {
@@ -18,5 +18,19 @@ export class Tape {
   moveHeadRight(): Tape {
     const newMiddle = (this.right.length == 0) ? this.blank : this.right[0]
     return new Tape(this.left + this.middle, newMiddle, this.right.substr(1, this.left.length - 1), this.blank)
+  }
+}
+
+export class TMConfiguration {
+  constructor(public state: number, public tape: Tape) {
+  }
+}
+
+export class TMRule {
+  constructor(private state: number, private character: string, private nextState: number, private writeCharacter: string, private direction: 'right' | 'left') {
+  }
+
+  isAppliesTo(configuration: TMConfiguration): boolean {
+    return ((this.state == configuration.state) && (this.character == configuration.tape.middle))
   }
 }
